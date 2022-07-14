077bdf1458e1e0adcac1f9405d6f18c9b22692af
Zachary Wolfson <zwolfson@hubspot.com>
2022-07-13 20:44:02 -0400

    eslint config added

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,14 @@
-const { createProxyMiddleware, fixRequestBody } = require('http-proxy-middleware');
+const {
+  createProxyMiddleware,
+  fixRequestBody,
+} = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:3001',
-            changeOrigin: true
-        })
-    );
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:3001",
+      changeOrigin: true,
+    })
+  );
 };
