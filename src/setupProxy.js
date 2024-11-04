ea21cf6f89e3e349e4b9b10022307927f3b205d9
zwolfson <zwolfson@hubspot.com>
2024-11-04 10:26:15 -0500

    added path to proxy to fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,14 @@
-// const {
-//   createProxyMiddleware,
-//   fixRequestBody,
-// } = require("http-proxy-middleware");
+const {
+  createProxyMiddleware,
+  fixRequestBody,
+} = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//   app.use(
-//     "/api",
-//     createProxyMiddleware({
-//       target: "http://127.0.0.1:3001",
-//       changeOrigin: true,
-//     })
-//   );
-// };
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://127.0.0.1:3001/api",
+      changeOrigin: true,
+    })
+  );
+};
